cc1c3d9a681288ce5567438698c1992e65d70e60
Harshit Raj Yadav <hy0098637@gmail.com>
2025-12-13 01:35:58 +0530

    axiosConfig file

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL, // MUST end with /api
+  baseURL: process.env.REACT_APP_API_URL, 
   headers: { "Content-Type": "application/json" }
 });
 
@@ -10,7 +10,9 @@ api.interceptors.request.use((config) => {
   const userInfo = localStorage.getItem("userInfo");
   if (userInfo) {
     const token = JSON.parse(userInfo)?.token;
-    if (token) config.headers.Authorization = `Bearer ${token}`;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
   }
   return config;
 });
